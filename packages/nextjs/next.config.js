68833266a346c56ea3501d2cc386c2c10340d65b
Crys <mariano.christopher123@gmail.com>
2023-06-19 21:56:55 +0800

    initial commit

---
+++ packages/nextjs/next.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // experimental: {
+  //   appDir: true,
+  // },
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+};
+
+module.exports = nextConfig;
